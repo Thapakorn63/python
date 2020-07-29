28a9a3688e883d77823849ce7a001d608cd42854
Thapakorn63 <68268428+Thapakorn63@users.noreply.github.com>
2020-07-29 20:47:39 +0700

    Add files via upload

---
+++ ดอกเบี้ย.py
@@ -0,0 +1,30 @@
+
+amount = int(input("Enter amount : "))
+rate = float(input("Enter rate : "))
+rate1 =rate/100
+year = int(input("Enter year : "))
+value = amount*(1+rate1)**year
+print("Future value = ",round(value,2)
+
+
+amount = amount*(1+rate1)
+print (("Year1  = "),round  (amount,2))
+amount = amount*(1+rate1)
+print (("Year2  = "),round (amount,2))
+amount = amount*(1+rate1)
+print (("Year3  = "),round (amount ,2))
+amount = amount*(1+rate1)
+print (("Year4  = "),round (amount,2))
+amount = amount*(1+rate1)
+print (("Year5  = "),round (amount,2))
+amount = amount*(1+rate1)
+print (("Year6  = "),round (amount,2))
+amount = amount*(1+rate1)
+print (("Year7  = "),round (amount,2))
+amount = amount*(1+rate1)
+print (("Year8  = "),round (amount,2))
+amount = amount*(1+rate1)
+print (("Year9  = "),round (amount,2))
+amount = amount*(1+rate1)
+print (("Year10 = "),round (amount,2))
+
